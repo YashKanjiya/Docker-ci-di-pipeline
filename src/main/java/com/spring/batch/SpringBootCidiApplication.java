83434968c6899099f5879f0155540d85e7ea8624
YASH KANJIYA <70482112+YashKanjiya@users.noreply.github.com>
2024-03-17 14:53:22 +0530

    add the data in git

---
+++ src/main/java/com/spring/batch/SpringBootCidiApplication.java
@@ -0,0 +1,13 @@
+package com.spring.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCidiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCidiApplication.class, args);
+	}
+
+}
